785451b1f5c3360f900bf905912bc24bbdd8cc8c
ArmandoArV <armando.arredondo.valle@gmail.com>
2025-04-22 16:14:35 -0600

    fixes main

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from src.app import app
 import os
 
 # Use environment variables for configuration
-HOST = "localhost"# os.getenv('FLASK_RUN_HOST', '0.0.0.0')
-PORT = 5000 # int(os.getenv('FLASK_RUN_PORT', 5000))
-DEBUG = False # os.getenv('FLASK_DEBUG', 'False').lower() in ['true', '1', 'yes']
-
-
+HOST = os.getenv('FLASK_RUN_HOST', '0.0.0.0')
+PORT = int(os.getenv('FLASK_RUN_PORT', 5000))
+DEBUG = os.getenv('FLASK_DEBUG', 'False').lower() in ['true', '1', 'yes']
 
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT, debug=DEBUG)
